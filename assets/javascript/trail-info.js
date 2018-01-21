f78dce920c82bd856f26e054af5825668421ee56
Molly Wojcik <mollywojcik@gmail.com>
2018-01-21 08:26:09 -0700

    testing click event listener for trail selection

--- assets/javascript/trail-info.js
+++ assets/javascript/trail-info.js
@@ -95,7 +95,7 @@ $(document).ready(function() {
 // END OLD FIREBASE CODE
 
   // AJAX call to get and write trail info upon clicking link from landing page
-  $(".card-action").on("click", function(event) {
+  $(".card trail").on("click", ".card-action", function(event) {
     console.log("My Test");
   });
 
